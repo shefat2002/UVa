06c1a66a93f664c7b3a5233039b6a0cd42aaef1d
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-24 00:27:22 +0600

    Create 11417 - GCD.cpp

---
+++ 11417 - GCD.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin >> n && n != 0){
+        int g = 0;
+        for(int i = 1 ; i< n;i++){
+            for(int j = i+1; j<=n;j++){
+                g += __gcd(i ,j);
+            }
+        }
+        cout << g << "\n";
+    }
+    return 0;
+}
