52a2f397be71eef501876f8b7418428c68897910
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-24 00:22:22 +0600

    Create 10469 - To Carry or not to Carry.cpp

---
+++ 10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    while(cin >> a >> b){
+
+        int ans ;
+        cout << (a ^ b) << endl;
+    }
+    return 0;
+}
