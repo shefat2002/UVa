1ca5168ddcf8fdfecaeb19871a3849d09a82c253
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-25 21:01:19 +0600

    Create 11044 - Searching for Nessy.cpp

---
+++ 11044 - Searching for Nessy.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl      cout << "\n";
+#define pi      (2*acos(0))
+#define case    cout << "Case " << case_number << ": "; case_number++;
+#define fast    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MAX_INT 1000002
+#define YES     "Yes"
+#define NO      "No"
+
+int case_number = 1;
+void file();
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+void solve()
+{
+     int a , b;
+     cin >> a >> b;
+     cout << (a/3)* (b/3);
+
+}
+
+
+int main()
+{
+    file();
+    int n; cin >> n;
+    while(n--){
+        //fast;
+        //case;
+        solve();
+        nl;
+    }
+    return 0;
+}
+
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r", stdin);
+    freopen("out.txt" , "w", stdout);
+    #endif
+}
