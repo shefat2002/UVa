489b5d7dd28b993635ba763a26a47f86740a9a82
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-25 21:38:51 +0600

    Create 11150 - Cola.cpp

---
+++ 11150 - Cola.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl      cout << "\n";
+#define pi      (2*acos(0))
+#define case    cout << "Case " << case_number << ": "; case_number++;
+#define fast    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MAX_INT 1000002
+#define YES     "Yes"
+#define NO      "No"
+
+int case_number = 1;
+void file();
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+void solve()
+{
+    int n; cin >> n;
+    int  count = 0;
+    while(n){
+        count += n;
+        n/=3;
+        count += n%3;
+    }
+    cout << count << endl;
+
+}
+
+
+int main()
+{
+    file();
+    //int n; cin >> n;
+    //while(n--){
+        //fast;
+        //case;
+        solve();
+    //    nl;
+    //}
+    return 0;
+}
+
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r", stdin);
+    freopen("out.txt" , "w", stdout);
+    #endif
+}
