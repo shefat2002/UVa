7b7734dc9b9a59c6ef88920b1cc1fc7b7f7c5d61
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-30 20:47:45 +0600

    Create 10683 - The decadary watch.cpp

---
+++ 10683 - The decadary watch.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define nl      cout << "\n";
+#define pi      (2*acos(0))
+#define case    cout << "Case " << case_number << ": "; case_number++;
+#define fast    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MAX_INT 1000002
+#define YES     "Yes"
+#define NO      "No"
+
+int case_number = 1;
+//knights move
+int dr[] = {2, 2, -2, -2, 1, 1, -1, -1};
+int dc[] = {1, -1, 1, -1, 2, -2, 2, -2};
+
+
+void file();
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+void solve()
+{
+    string s;
+    while(cin >> s){
+        int h, min , sec , c;
+        h = (s[0]-'0')*10+ (s[1]-'0');
+        min = (s[2]-'0')*10+ (s[3]-'0');
+        sec = (s[4]-'0')*10+ (s[5]-'0');
+        c = (s[6]-'0')*10+ (s[7]-'0');
+        long long time = c *10 + sec * 100 + min * 60000 + h*3600000;
+        h = time / 8640000;
+        time %= 8640000;
+        min = time/86400;
+        time %= 86400;
+        sec = time/864;
+        time %= 864;
+        c = time*100/864;
+        cout << setfill('0') << setw(2)<< h;
+        cout << setfill('0') << setw(2)<< min;
+        cout << setfill('0') << setw(2)<< sec;
+        cout << setfill('0') << setw(2)<< c;
+        nl;
+    }
+}
+
+
+int main()
+{
+    file();
+    //int n; cin >> n;
+    //while(n--){
+        //fast;
+        //case;
+        solve();
+    //    nl;
+    //}
+    return 0;
+}
+
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r", stdin);
+    freopen("out.txt" , "w", stdout);
+    #endif
+}
