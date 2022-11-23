7505fe459d43f71b8ecba6fcb62ee5857fe66330
Shefat Al Mahmud <shefat2002@gmail.com>
2022-11-24 00:33:35 +0600

    Update 957 - Popes.cpp

--- 957 - Popes.cpp
+++ 957 - Popes.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cctype>
+#include <limits>
 using namespace std;
-
 #define ll      long long
 #define nl      cout << "\n";
 #define pi      (2*acos(0))
@@ -13,47 +29,49 @@ using namespace std;
 
 int case_number = 1;
 //knights move
-int dx[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-int dy[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+int dr[] = {2, 2, -2, -2, 1, 1, -1, -1};
+int dc[] = {1, -1, 1, -1, 2, -2, 2, -2};
 
 
 void file();
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
 
+
 void solve()
 {
-    int y;
-    while(cin >> y){
-        int p; cin >> p;
-        vector<int>v;
-        for(int i= 0 ; i< p ; i++){
-            int x;cin >>x;
-            v.push_back(x);
-        }
-        int first , last, mx=-1 , diff=0;
-        for(int i = 0 ; i < p ; i++){
-            while(v[i]-v[diff] >=y) diff++;
-            if(mx < i-diff+1){
-                mx = i-diff+1;
-                first = diff;
-                last = i;
+    int y,p ; 
+    while(cin >>  y>> p){
+        vector<int> a(p);
+        int cnt=0, mx =0, diff =0, first, last;
+        for(int i = 0 ; i< p ; i++){
+            cin >> a[i];
+            cnt++;
+            if(i>0){
+                while(a[i] - a[diff] >=y){
+                    diff++;
+                    cnt--;
+                }
+                if(cnt > mx){
+                    mx = cnt;
+                    first = a[diff];
+                    last = a[i];
+                }
             }
         }
-        cout << mx << ' '<< v[first] << ' ' << v[last];nl;
+        cout << mx << ' ' << first << ' ' << last;nl;
     }
-    
-
 
 }
+
 
 int main()
 {
     file();
     int n; 
     solve();
-    //cin >> n; while(n--){solve();nl}       //CF
-    //cin >> n; while(n--){case;solve();nl;} // Case 
-    //cin >> n; while(n--){caseh;solve();nl;} // Case (Case #01: )
+    //cin >> n; while(n--){solve();nl;}
+    //cin >> n; while(n--){case;nl;solve();nl;} // Case 
+    //cin >> n; while(n--){caseh;solve();nl;} // Case (Case #1: )
     return 0;
 }
 
