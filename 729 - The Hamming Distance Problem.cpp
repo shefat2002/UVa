ae1d77a1055b197e0b41c6638ecf4d8773541656
Shefat Al Mahmud <shefat2002@gmail.com>
2023-02-14 19:04:36 +0600

    update

--- 729 - The Hamming Distance Problem.cpp
+++ 729 - The Hamming Distance Problem.cpp
@@ -1,68 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll      long long
 #define nl      cout << "\n";
-#define pi      (2*acos(0))
-#define case    cout << "Case " << case_number << ": "; case_number++;
-#define fast    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define MAX_INT 1000002
-#define YES     "Yes"
-#define NO      "No"
 
-int case_number = 1;
-//knights move
-int dr[] = {2, 2, -2, -2, 1, 1, -1, -1};
-int dc[] = {1, -1, 1, -1, 2, -2, 2, -2};
-
-
-void file();
-/*----------------------------------------------------------------------------------------------------------------------------------------*/
-
-vector<vector<int>> ans;
-
-void permute(int a[] , int n)
+void permute(string s,int i,int l,int h,int n)
 {
-    do{
-        for(int i = 0 ; i < n ;i++) cout << a[i];nl;
-    }while(next_permutation(a,a+n));
+    if(i==n){
+        if(h==l){cout << s;nl;}
+        return;
+    }
+    s+='0';
+    permute(s,i+1,l,h,n);
     
+    s[i]='1';
+    permute(s,i+1,l+1,h,n);
+
 }
 
 void solve()
 {
-    int n, h; cin >> n >> h;
-    int ham[n];
-    for(int i = 0 ; i< n -h ;i++){
-        ham[i] = 0;
-    }
-    for(int i = n-h; i< n ; i++){
-        ham[i] = 1;
-    }
-    permute(ham, n);
-
-    
+    int n,h;cin >>  n >> h;
+    string s="";
+    permute(s,0,0,h,n);
 }
-
 
 int main()
 {
-    file();
     int n; cin >> n;
     
     while(n--){ 
-        //fast;
-        //case;
         solve();
         if(n>0)nl;
     }
     return 0;
 }
 
-void file()
-{
-    #ifndef ONLINE_JUDGE
-    freopen("in.txt" , "r", stdin);
-    freopen("out.txt" , "w", stdout);
-    #endif
-}
